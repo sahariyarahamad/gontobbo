ac1971762306e21b0a32a97d6082d80b787ea4fa
sahariyarahamad <djsahariyaahammed@gmail.com>
2025-12-06 21:31:15 +0600

    added

---
+++ script.js
@@ -0,0 +1,37 @@
+
+document.getElementById("icon-linkedin").src = "https://www.google.com/s2/favicons?sz=64&domain=linkedin.com";
+document.getElementById("icon-facebook").src = "https://www.google.com/s2/favicons?sz=64&domain=facebook.com";
+document.getElementById("icon-github").src = "https://www.google.com/s2/favicons?sz=64&domain=github.com";
+
+document.getElementById("img-app-current-version").src="https://img.shields.io/github/v/release/sahariyarahamad/alnova-news-app";
+
+document.getElementById("link-linkedin").href = "https://www.linkedin.com/in/sahariyarahamadbd";
+document.getElementById("link-github").href = "https://www.github.com/sahariyarahamad/";
+document.getElementById("link-facebook").href = "https://www.facebook.com/sahariyarahamadbd/";
+
+const latestVersionURL = "#"
+
+document.getElementById("app-download").href = latestVersionURL;
+document.getElementById("app-download-bottom").href = latestVersionURL;
+
+
+
+// for tester version and desc
+document.getElementById("tv-tester-version").textContent = "Tester version: 1.2 - Beta";
+document.getElementById("tv-tester-description").textContent = "Description: Some bug fixed";
+document.getElementById("tester-app-download").href = "https://www.google.com";
+
+const isTesterShow = true;
+const params = new URLSearchParams(window.location.search);
+
+if (isTesterShow){
+
+    if (params.has('t')){
+        document.getElementById('section-tester').style.display = "block";
+        
+    }else{
+        document.getElementById('section-tester').style.display = "none";
+    }
+}else{
+    document.getElementById('section-tester').style.display = "none";
+}
