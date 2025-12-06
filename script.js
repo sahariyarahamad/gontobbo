d9871ab608693a185948ff4a3000116b428f1f38
Sahariyar Ahamad <djsahariyaahammed@gmail.com>
2025-12-06 21:38:01 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+document.getElementById("icon-linkedin").src = "https://www.google.com/s2/favicons?sz=64&domain=linkedin.com";
+document.getElementById("icon-facebook").src = "https://www.google.com/s2/favicons?sz=64&domain=facebook.com";
+document.getElementById("icon-github").src = "https://www.google.com/s2/favicons?sz=64&domain=github.com";
+
+document.getElementById("img-app-current-version").src="https://img.shields.io/github/v/release/sahariyarahamad/alnova-news-app";
+
+document.getElementById("link-linkedin").href = "https://www.linkedin.com/in/sahariyarahamadbd";
+document.getElementById("link-github").href = "https://www.github.com/sahariyarahamad/";
+document.getElementById("link-facebook").href = "https://www.facebook.com/sahariyarahamadbd/";
+
+const latestVersionURL = "#"
+
+document.getElementById("app-download").href = latestVersionURL;
+document.getElementById("app-download-bottom").href = latestVersionURL;
+
+const params = URLSearchParams(Window.location.search);
+
+if (params.has('t')){
+    document.getElementById('tt').style.display = "block";
+    
+}
